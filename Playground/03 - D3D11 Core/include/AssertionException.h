66744cf9bddd34316895be00fc399458255feb3a
Adam Byrd <sycobob@gmail.com>
2015-12-18 09:17:53 -0600

    Partial progress on AssertionException

--- Playground/03 - D3D11 Core/include/AssertionException.h
+++ Playground/03 - D3D11 Core/include/AssertionException.h
@@ -1,7 +1,19 @@
 #pragma once
 
-#define throw_assert(x) do { if ( !(x) ) { /**/ } } while (0)
+#define throw_assert(x, msg)                     \
+do { if ( !(x) )                                 \
+{                                                \
+	throw AssertionException(#x, msg,            \
+	          __FILE__, __LINE__, __FUNCTION__); \
+} } while (0)
 
 class AssertionException : public std::exception
 {
+public:
+	AssertionException(std::string, std::string, const char*, DWORD, const char*);
+
+	virtual const char* what() const;
+
+private:
+	std::string message;
 };
--- Playground/03 - D3D11 Core/src/AssertionException.cpp
+++ Playground/03 - D3D11 Core/src/AssertionException.cpp
@@ -1,2 +1,21 @@
 #include "stdafx.h"
 #include "AssertionException.h"
+
+using namespace std;
+
+AssertionException::AssertionException(
+	string      expression,
+	string      message,
+	const char* file,
+	DWORD       line,
+	const char* function)
+{
+	ostringstream msgStream;
+	msgStream << "Assertion failed: " << expression << endl;
+	          //<< 
+}
+
+const char* AssertionException::what() const
+{
+	return message.c_str();
+}
